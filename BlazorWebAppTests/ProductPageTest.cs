a96895674a066b32f7fa9b45c5d903ab41efae31
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-25 20:48:48 +0100

    refactor: improve comments

--- BlazorWebAppTests/ProductPageTest.cs
+++ BlazorWebAppTests/ProductPageTest.cs
@@ -9,10 +9,10 @@ public class ProductsPageTest : BunitContext
     [Fact]
     public void ProductPage_Shows_Products_Corretly()
     {
-        // Act - bUnit will call OnInitialized automaticall, products list gets populated
+        // Arrange - bUnit will call OnInitialized automaticall, products list gets populated
         var cut = Render<Home>();
 
-        // Find all <li> elements (product names)
+        // Act - Find all <li> elements (product names)
         var productItems = cut.FindAll("li");
 
         // Assert - Here we are using xUnit Assertion syntax
