2839d967c4a4ea73dcde3a7cb5329930678e405e
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-25 20:41:02 +0100

    feat: add counter test

---
+++ BlazorWebAppTests/CounterTest.cs
@@ -0,0 +1,26 @@
+using Bunit;
+using Xunit;
+using BlazorWebApp.Components;
+
+namespace BlazorWebAppTests.Components;
+public class CounterTest: BunitContext
+{
+    [Fact]
+    public void Counter_Increments_WhenButtonIsClicked()
+    {
+        // Arrange
+        var cut = Render<Counter>();
+
+        // Act
+        cut.Find("button").Click();
+
+        // Assert
+        
+        cut.MarkupMatches("""
+        <h1>Counter</h1>
+        <p>Count: 2</p>
+        <button>Click me</button>
+        """);
+    }
+
+}
